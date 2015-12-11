4cde6fe3c8788b5b22f6adc2cc3e757d2f4b0062
dewlyer <liluak@yeah.net>
2015-12-11 16:25:17 +0800

    add grunt

--- js/app/prototype.js
+++ js/app/prototype.js
@@ -2,48 +2,47 @@ define(['jquery'], function($){
     return {
         init: function() {
             var self = this;
-            self.appoint = new Date(this.settings.appoint);
-            self.current = new Date(this.settings.current);
+            self.appoint = new Date(self.settings.appoint);
+            self.current = new Date(self.settings.current);
             self.build();
-            self.calculate();
             self.render();
-            setInterval(function(){
-                self.refresh();
-            }, 1000);
-        },
-        calculate: function(current) {
-            current = current || this.current;
-            var totalMsec = Math.abs(current - this.appoint);
-            var totalSec = totalMsec / 1000;
-            this.seconds = Math.floor(totalSec) % 60;
-            this.minutes = Math.floor(totalSec/60) % 60;
-            this.hours = Math.floor(totalSec/3600) % 24;
-            this.days = Math.floor(totalSec/86400);
-            //this.days = parseInt(this.totalSec / 3600 / 24);
-            //this.hours = parseInt((this.totalSec - this.days*24*3600) / 3600);
-            //this.minutes = parseInt((this.totalSec - this.days*24*3600 - this.hours*3600) / 60);
-            //this.seconds = parseInt(this.totalSec - this.days*24*3600 - this.hours*3600 - this.minutes*60);
         },
         build: function() {
-            this.$days = $(this.settings.days);
-            this.$hours = $(this.settings.hours);
-            this.$minutes = $(this.settings.minutes);
-            this.$seconds = $(this.settings.seconds);
-            $(this.element).append(this.$days).append(this.$hours).append(this.$minutes).append(this.$seconds);
+            var self = this;
+            self.$days = $(self.settings.days);
+            self.$hours = $(self.settings.hours);
+            self.$minutes = $(self.settings.minutes);
+            self.$seconds = $(self.settings.seconds);
+            $(self.element).append(self.$days).append(self.$hours).append(self.$minutes).append(self.$seconds);
+            self.calculate();
+        },
+        calculate: function(current) {
+            var self = this;
+            current = current || self.current;
+            var totalMsec = Math.abs(current - self.appoint);
+            var totalSec = totalMsec / 1000;
+            self.seconds = Math.floor(totalSec) % 60;
+            self.minutes = Math.floor(totalSec/60) % 60;
+            self.hours = Math.floor(totalSec/3600) % 24;
+            self.days = Math.floor(totalSec/86400);
+            //this.days = parseInt(this.totalSec / 3600 / 24);this.hours = parseInt((this.totalSec - this.days*24*3600) / 3600);this.minutes = parseInt((this.totalSec - this.days*24*3600 - this.hours*3600) / 60);this.seconds = parseInt(this.totalSec - this.days*24*3600 - this.hours*3600 - this.minutes*60);
         },
         render: function() {
-            this.renderUnit(this.$days, this.days, this.settings.marker.days);
-            this.renderUnit(this.$hours, this.hours, this.settings.marker.hours);
-            this.renderUnit(this.$minutes, this.minutes, this.settings.marker.minutes);
-            this.renderUnit(this.$seconds, this.seconds, this.settings.marker.seconds);
-
-            $(this.element).css({
-                'width':        $(this.element).outerWidth(),
-                'margin-top':   -$(this.element).height() / 2,
-                'margin-left':  -$(this.element).width() / 2,
+            var self = this;
+            self.renderUnit(self.$days, self.days, self.settings.marker.days);
+            self.renderUnit(self.$hours, self.hours, self.settings.marker.hours);
+            self.renderUnit(self.$minutes, self.minutes, self.settings.marker.minutes);
+            self.renderUnit(self.$seconds, self.seconds, self.settings.marker.seconds);
+            $(self.element).css({
+                'width':        $(self.element).outerWidth(),
+                'margin-top':   -$(self.element).height() / 2,
+                'margin-left':  -$(self.element).width() / 2,
                 'left':         '50%',
                 'top':          '50%'
             });
+            self.timer = setInterval(function(){
+                self.refresh();
+            }, 1000);
         },
         renderUnit: function(obj, val, mark) {
             var $unitSet = $('<div class="unit-set"></div>');
@@ -63,25 +62,24 @@ define(['jquery'], function($){
         refresh: function(){
             var self = this;
             self.calculate(Date.now());
-            self.refreshUnit(self.$days, self.days);
-            self.refreshUnit(self.$hours, self.hours);
-            self.refreshUnit(self.$minutes, self.minutes);
+            //self.refreshUnit(self.$days, self.days);
+            //self.refreshUnit(self.$hours, self.hours);
+            //self.refreshUnit(self.$minutes, self.minutes);
             self.refreshUnit(self.$seconds, self.seconds);
         },
         refreshUnit: function(obj, val){
             var numbers = this.splitNum(val);
             for(var j in numbers) {
-                //obj.find('.unit-set').eq(j).find('.unit').removeClass('previous').eq(numbers[(j-1)]).addClass('previous');
                 var $units = obj.find('.unit-set').eq(j).find('.unit');
                 $units.removeClass('active previous');
+                $units.eq(numbers[j]-1).addClass('previous');
                 $units.eq(numbers[j]).addClass('active');
-                var k = j>numbers.length ? j-1 : 0;
-                $units.eq(numbers[k]).addClass('previous');
 
                 //obj.find('.unit-set').eq(j).find('.unit').removeClass('active').eq(numbers[j]).addClass('active')
                 //    .prev('.unit').addClass('previous');
+                //console.log('j is %O, k is %O', $units.eq(numbers[j]), $units.eq(numbers[j]-1));
+                //debugger;
             }
-            //debugger;
         },
         splitNum: function(val) {
             var numbs = [], digit = 0;
--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ requirejs.config({
 requirejs(['app'], function(){
     var $countdown = $('<div id="countdown" class="countdown"></div>');
     $countdown.prependTo(document.body).countdown({
-        appoint: '1988/09/07 00:00:00',
-        //appoint: '2016/01/18 00:00:00',
+        //appoint: '1988/09/07 00:00:00',
+        appoint: '2016/01/18 00:00:00',
         auto: false,
         marker: {
-            seconds:    '秒',
-            minutes:    '分',
-            hours:      '时',
-            days:       '天'
+            seconds:    '\u79D2',
+            minutes:    '\u5206',
+            hours:      '\u65F6',
+            days:       '\u5929'
         }
     });
 });
